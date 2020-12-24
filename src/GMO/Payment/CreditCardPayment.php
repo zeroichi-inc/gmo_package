dd0895f08063459f960e5c36d7c17a3b53e5f9cd
Roberth Rojas <40683721+rorojasq@users.noreply.github.com>
2020-12-24 01:10:31 -0600

    Initialize credentials for credit card

--- src/GMO/Payment/CreditCardPayment.php
+++ src/GMO/Payment/CreditCardPayment.php
@@ -4,4 +4,27 @@ namespace GMO\Payment;
 
 class CreditCardPayment
 {
+    private string $host;
+
+    private string $siteId = "";
+    private string $sitePass = "";
+
+    private string $shopId = "";
+    private string $shopPass = "";
+
+    public function __construct(string $host, array $credentials)
+    {
+        if (array_key_exists('siteId', $credentials)) {
+            $this->siteId = $credentials['siteId'];
+        }
+        if (array_key_exists('sitePass', $credentials)) {
+            $this->sitePass = $credentials['sitePass'];
+        }
+        if (array_key_exists('shopId', $credentials)) {
+            $this->shopId = $credentials['shopId'];
+        }
+        if (array_key_exists('shopPass', $credentials)) {
+            $this->shopPass = $credentials['shopPass'];
+        }
+    }
 }
