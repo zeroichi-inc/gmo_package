7da5dbd796177acfd7e676d77ee3772c8c5730f6
Roberth Rojas <40683721+rorojasq@users.noreply.github.com>
2020-12-24 02:28:10 -0600

    Change shop and site id case

--- src/GMO/Payment/CreditCardPayment.php
+++ src/GMO/Payment/CreditCardPayment.php
@@ -2,26 +2,32 @@
 
 namespace GMO\Payment;
 
+use GMO\Payment\Api;
+
 class CreditCardPayment
 {
     private string $host;
 
-    private string $siteId = "";
+    private string $siteID = "";
     private string $sitePass = "";
 
-    private string $shopId = "";
+    private string $shopID = "";
     private string $shopPass = "";
+
+    private array $apiParams = [];
 
     public function __construct(string $host, array $credentials)
     {
-        if (array_key_exists('siteId', $credentials)) {
-            $this->siteId = $credentials['siteId'];
+        $this->host = $host;
+
+        if (array_key_exists('siteID', $credentials)) {
+            $this->siteID = $credentials['siteID'];
         }
         if (array_key_exists('sitePass', $credentials)) {
             $this->sitePass = $credentials['sitePass'];
         }
-        if (array_key_exists('shopId', $credentials)) {
-            $this->shopId = $credentials['shopId'];
+        if (array_key_exists('shopID', $credentials)) {
+            $this->shopID = $credentials['shopID'];
         }
         if (array_key_exists('shopPass', $credentials)) {
             $this->shopPass = $credentials['shopPass'];
