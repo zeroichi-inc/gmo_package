5c8dff1dcfad451f1a0c1b8abd5eb74e5bde81b5
Roberth Rojas <40683721+rorojasq@users.noreply.github.com>
2021-12-29 03:47:11 -0600

    Add support for order idPass api

--- src/GMO/Payment/Api.php
+++ src/GMO/Payment/Api.php
@@ -31,12 +31,17 @@ class Api
     public const JOBCD_SALES = 'SALES';
     public const JOBCD_CANCEL = 'CANCEL';
 
+    public const API_JSON = 'api_json';
+    public const API_IDPASS = 'api_idpass';
+
     protected string $apiBaseUrl;
     protected array $params = array();
+    protected string $apiType;
 
-    public function __construct(string $apiBaseUrl)
+    public function __construct(string $apiBaseUrl, string $apiType = self::API_JSON)
     {
         $this->apiBaseUrl = $apiBaseUrl;
+        $this->apiType = $apiType;
     }
 
     public function setParam(string $name, $value)
@@ -57,6 +62,15 @@ class Api
     }
 
     public function request(string $apiMethod)
+    {
+        if ($this->apiType == self::API_JSON) {
+            return $this->requestJson($apiMethod);
+        } else if ($this->apiType == self::API_IDPASS) {
+            return $this->requestIdpass($apiMethod);
+        }
+    }
+
+    public function requestJson(string $apiMethod)
     {
         $methodUrl = $this->apiBaseUrl . '/' . $apiMethod . '.json';
 
@@ -86,5 +100,84 @@ class Api
         );
 
         return $response;
+    }
+
+    public function requestIdpass(string $apiMethod)
+    {
+        $methodUrl = $this->apiBaseUrl . '/' . $apiMethod . '.idPass';
+
+        $ch = curl_init();
+
+        $opts = array(
+            CURLOPT_URL => $methodUrl,
+            CURLOPT_POSTFIELDS => $this->getUrlEncodedParams(),
+            CURLOPT_HTTPHEADER => array('Content-Type: application/x-www-form-urlencoded;charset=windows-31j'),
+            CURLOPT_RETURNTRANSFER => true,
+            CURLINFO_HEADER_OUT => true, // Track request headers
+        );
+        curl_setopt_array($ch, $opts);
+
+        $curlres = curl_exec($ch);
+        $curlinfo = curl_getinfo($ch);
+        curl_close($ch);
+
+        // In case cURL fails to connect
+        if ($curlres === false) {
+            return NULL;
+        }
+
+        $response = array(
+            'status' => $curlinfo['http_code'],
+            'response' => $this->convertIDPassToJson($curlres),
+        );
+
+        return $response;
+    }
+
+    protected function getUrlEncodedParams()
+    {
+        $camelCaseParams = array();
+
+        foreach($this->params as $key => $value) {
+            $key[0] = strtoupper($key[0]);
+            $camelCaseParams[$key] = $value;
+        }
+
+        // The old api requires Shift-JIS (Windows31j) encoding
+        return mb_convert_encoding(http_build_query($camelCaseParams), 'SJIS');
+    }
+
+    protected function convertIDPassToJson(string $idPassRes)
+    {
+        $res = array();
+
+        // Convert the response to utf-8
+        $idPassRes = mb_convert_encoding($idPassRes, 'UTF-8', 'SJIS');
+        parse_str($idPassRes, $parsedIdPassRes);
+
+        // Change keys casing
+        foreach ($parsedIdPassRes as $key => $value) {
+            $key[0] = strtolower($key[0]);
+
+            if (strpos($value, '|') !== false) {
+                $values = explode('|', $value);
+
+                foreach($values as $index => $val) {
+                    if (is_string($val) && strlen($val) == 0) {
+                        $val = NULL;
+                    }
+
+                    $res[$index][$key] = $val;
+                }
+            } else {
+                if (is_string($value) && strlen($value) == 0) {
+                    $value = NULL;
+                }
+
+                $res[$key] = $value;
+            }
+        }
+
+        return $res;
     }
 }
