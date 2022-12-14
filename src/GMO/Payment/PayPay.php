8ffdbfdf439627b300240d7282d5583a31729636
Roberth Rojas <40683721+rorojasq@users.noreply.github.com>
2022-12-13 23:09:29 -0600

    Implement `entryTranPaypay` method

--- src/GMO/Payment/PayPay.php
+++ src/GMO/Payment/PayPay.php
@@ -4,6 +4,9 @@ namespace GMO\Payment;
 
 class PayPay extends Api
 {
+    use SetCredentials;
+
+
     private const METHOD_ENTRY_TRAN_PAYPAY = 'EntryTranPaypay';
     private const METHOD_EXEC_TRAN_PAYPAY = 'ExecTranPaypay';
     private const METHOD_PAYPAY_START = 'PaypayStart';
@@ -11,33 +14,23 @@ class PayPay extends Api
     private const METHOD_PAYPAY_CANCEL_RETURN = 'PaypayCancelReturn';
 
 
-    private string $siteID = "";
-    private string $sitePass = "";
-
-    private string $shopID = "";
-    private string $shopPass = "";
-    
-
     public function __construct(string $host, array $credentials, bool $forceOldApi = false)
     {
         parent::__construct($host, $this->apiType = $forceOldApi? self::API_IDPASS: self::API_JSON);
 
-        if (array_key_exists('siteID', $credentials)) {
-            $this->siteID = $credentials['siteID'];
-        }
-        if (array_key_exists('sitePass', $credentials)) {
-            $this->sitePass = $credentials['sitePass'];
-        }
-        if (array_key_exists('shopID', $credentials)) {
-            $this->shopID = $credentials['shopID'];
-        }
-        if (array_key_exists('shopPass', $credentials)) {
-            $this->shopPass = $credentials['shopPass'];
-        }
+        $this->receiveCredentials($credentials);
     }
 
     public function entryTranPaypay(string $orderID, int $amount, int $tax, string $jobCd = self::JOBCD_AUTH)
     {
+        $this->setShopCredentials();
+
+        $this->setParam('orderID', $orderID);
+        $this->setParam('jobCd', $jobCd);
+        $this->setParam('amount', $amount);
+        if ($tax > 0) $this->setParam('tax', $tax);
+
+        return $this->request(self::METHOD_ENTRY_TRAN_PAYPAY);
     }
 
     public function execTranPaypay(
